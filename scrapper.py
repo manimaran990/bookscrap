ac5d8fa56825aba7f3fa5a52189920f513a63974
m4n1g <manimaran990@gmail.com>
2020-11-07 14:05:50 -0500

    getting title from h4 tag and few changes

--- scrapper.py
+++ scrapper.py
@@ -8,11 +8,8 @@ import csv
 import re
 import multiprocessing
 
-
 BASE_URL = 'http://www.noolulagam.com/books'
-prohibitedWords = ['வகை', 'பதிப்பகம்', 'எழுத்தாளர்', 'விலை', '(Login for special discount)']
-HEADER = ['நூல் பெயர்', 'வகை', 'எழுத்தாளர்', 'பதிப்பகம்', 'விலை']
-remove_words = re.compile('|'.join(map(re.escape, prohibitedWords)))
+HEADER = ['நூல் பெயர்', 'வகை', 'எழுத்தாளர்', 'பதிப்பகம்', 'Year', 'விலை']
 
 def get_data(n):
 	res_lst = []
@@ -20,23 +17,25 @@ def get_data(n):
 		url = f"{BASE_URL}/{str(n)}"
 		page = requests.get(url)
 		soup = BeautifulSoup(page.content, 'html.parser')
-		t = soup.findAll('table')
-		for tab in t:
-			for line in tab:
-				if 'வகை' in str(line) and 'பதிப்பகம்' in str(line) and 'எழுத்தாளர்' in str(line) and 'விலை' in str(line):
-					title = line.findAll('a')[-1]['title'].replace('Buy tamil book ','')
-					cols = line.find('td', align='left')
-					cols = [ ele.text.strip() for ele in cols ]
-					vals = remove_words.sub('', str(cols)).replace(': ', ',')[3:].split(',')
-					price = vals[-1].replace('\']', '').strip()
-					vals.insert(0, title)
-					vals[-1] = price
-					vals = [ v.strip() for v in vals ] #trim whitespaces
-					res_lst.append(vals)
-	except:
-		pass
+		tabs = soup.find_all('table', attrs={ 'style':"border-collapse: collapse; border: 0px solid orange; width :100%"} )
+		for t in tabs:
+			keys = []
+			val = []
+			title = {'நூல் பெயர்': t.find('h4').get_text().strip() } #getting title from h4
+			cols= t.find('table').findAll('td', align='left')
+			cols = [ ele.text.strip() for ele in cols ]
+			for c in range(len(cols)):
+				if c%2 == 0:
+					keys.append(cols[c])
+				else:
+					val.append(cols[c])
+			fin_dict = dict(zip(keys,val))
+			fin_dict.update(title)
+			res_lst.append(fin_dict)
+	except Exception as e:
+		print(f"error occurred {str(e)}")
 	return res_lst 
-                
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -47,8 +46,8 @@ if __name__ == '__main__':
 
 	with multiprocessing.Pool() as p:
 		with open(args.out, 'a') as csvfile:
-			writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-			#writer.writerow(HEADER)
+			writer = csv.DictWriter(csvfile, fieldnames=HEADER, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			writer.writeheader()
 			for n in tqdm(p.imap_unordered(get_data, range(args.start, args.end+1))):
 				for item in n:
 					writer.writerow(item)
